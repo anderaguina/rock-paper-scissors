703ca5d63d0c15a20a4bd53dda63a5dbfc5e4b9f
Ander Aguinagalde <ander@tisalabs.com>
2020-11-30 13:24:04 +0000

    Show who wins, keep score and show what each one has selected

--- src/components/GameDashboard.vue
+++ src/components/GameDashboard.vue
@@ -5,28 +5,70 @@
         <div class="card">
           <div class="card-header"> Rock, paper, scissors </div>
           <div class="card-body">
+            Select an option to play!
             <div id="game" class="text-center">
               <!-- DISPLAY GAME BUTTONS -->
-              <div class="d-inline mr-3">
-                <button>
+              <div id="rock" class="d-inline mr-3">
+                <button v-on:click="play(0)">
                   <img src="../assets/rock.png">
                 </button>
               </div>
-              <div class="d-inline mr-3">
-                <button>
+              <div id="paper" class="d-inline mr-3">
+                <button v-on:click="play(1)">
                   <img src="../assets/paper.png">
                 </button>
               </div>
-              <div class="d-inline">
-                <button>
+              <div id="scissors" class="d-inline">
+                <button v-on:click="play(2)">
                   <img src="../assets/scissors.png">
                 </button>
               </div>
             </div>
 
+            <!-- DISPLAY MATCH -->
+            <div id="match" v-if="matching">
+              <h4> Match </h4>
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col"> Your option</th>
+                    <th scope="col"> Computer option </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td scope="row">
+                      <span> You have selected {{userOption}} </span>
+                    </td>
+                    <td>
+                      <span> Computer has selected {{computerOption}} </span>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+
             <!-- DISPLAY GAME RESULTS -->
             <div id="results">
-
+              <h4> Results </h4>
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th scope="col">  </th>
+                    <th scope="col"> Score </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row"> You </th>
+                    <td> {{user.data.displayName}}'s score is {{userScore}} </td>
+                  </tr>
+                  <tr>
+                    <th scope="row"> Computer </th>
+                    <td> User score is {{computerScore}} </td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
@@ -36,20 +78,84 @@
 </template>
 
 <script>
-
+  import { mapGetters } from "vuex";
   export default {
     data () {
       return {
+        userScore: 0,
+        computerScore: 0,
+        userOption: "",
+        computerOption: "",
+        matching: false,
+        userSelectedOption: 0,
+        computerSelectedOption: 0,
       }
     },
     methods: {
-      
+      play(option) {
+        this.userSelectedOption = option;
+        console.log("OPTION ", option);
+        let computerOption = this.generateComputerOption();
+        this.computerSelectedOption = computerOption;
+        this.showMatch();
+        
+        setTimeout(this.resolveMatchup, 1000);
+      },
+      generateComputerOption() {
+        let random = Math.floor(Math.random() * 3);
+        return random;        
+      },
+      showMatch() {
+        this.matching = true;
+        let options = ["Rock", "Paper", "Scissors"]
+
+        this.userOption = options[this.userSelectedOption];
+        this.computerOption = options[this.computerSelectedOption];        
+      },
+
+      resolveMatchup() {
+        this.matching = false;
+        if (this.userSelectedOption == 0) {
+          if (this.computerSelectedOption == 1) {
+            alert("OH NO, YOU LOSE!");
+            this.computerScore++;
+          } else if (this.computerSelectedOption == 2){
+            alert("CONGRATULATIONS!");
+            this.userScore++;
+          } else {
+            alert("IT'S A DRAW!");
+          }
+        } else if (this.userSelectedOption == 1) {
+          if (this.computerSelectedOption == 2) {
+            alert("OH NO, YOU LOSE!");
+            this.computerScore++;
+          } else if (this.computerSelectedOption == 0){
+            alert("CONGRATULATIONS!");
+            this.userScore++;
+          } else {
+            alert("IT'S A DRAW!");
+          }
+        } else {
+          if (this.computerSelectedOption == 0) {
+            alert("OH NO, YOU LOSE!");
+            this.computerScore++;
+          } else if (this.computerSelectedOption == 1){
+            alert("CONGRATULATIONS!");
+            this.userScore++;
+          } else {
+            alert("IT'S A DRAW!");
+          }
+        } 
+      }
     },
     created: function () {
       
     },
     computed: {
-      
+      ...mapGetters({
+        // map `this.user` to `this.$store.getters.user`
+        user: "user"
+    })
     }
   };
 </script>
